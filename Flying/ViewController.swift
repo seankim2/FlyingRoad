85a3737f13c3a2b5657faf665d9ab2ad48c0d6e9
seankim2 <khkim2010@gmail.com>
2019-01-01 19:24:47 +0900

    - initial commit

---
+++ Flying/ViewController.swift
@@ -0,0 +1,28 @@
+ //
+//  ViewController.swift
+//  Flying
+//
+//  Created by KwangHo Kim on 2018. 11. 17..
+//  Copyright © 2018년 WizardFactory. All rights reserved.
+//
+
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
